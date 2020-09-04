9c031f3dd58c68a0ff5e97dad000dbde711f3ca4
IdaliaGpe <idaliaguadalupepucca@hotmail.com>
2020-09-04 09:01:12 -0700

    hero

--- src/main.cc
+++ src/main.cc
@@ -1,10 +1,13 @@
 #include<iostream>
-#include "Character.hh"
+//#include "Character.hh"
+#include "Hero.hh"
 
 int main()
 {
 
-    Character* character1{new Character(5, 3, 1, 2, 3)};
+    Hero* hero1{new Hero(5, 3, 1, 2, 3)};
+    /*Character* character1{new Character(5, 3, 1, 2, 3)};
+
     std::cout << "Character info:" << std::endl;
     std::cout << "Health: " << character1->GetHealth() << std::endl;
     std::cout << "Exp: " << character1->GetExp() << std::endl;
@@ -14,9 +17,9 @@ int main()
     character1->SetLore("la chinita en el bosque se perdio");
     character1->GetDamage(2);
     std::cout << "Health: " << character1->GetHealth() << std::endl;
-    std::cout << "Lore: " << character1->GetLore() << std::endl;
-    delete &character1;
-    
+    std::cout << "Lore: : " << character1->GetLore() << std::endl;
+    delete &character1;*/
+
     std::cin.get();
     return 0;
 }
